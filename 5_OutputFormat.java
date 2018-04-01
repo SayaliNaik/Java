9286efe0a04aaa197e30baebe837dd3bb990d0e6
Sayali Naik <sayalianaik21@gmail.com>
2018-04-01 14:41:18 -0700

    formatting output using printf

---
+++ 5_OutputFormat.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import java.text.DecimalFormat;
+//using printf
+
+public class Solution {
+
+    public static void main(String[] args) {
+            Scanner sc=new Scanner(System.in);
+            System.out.println("================================");
+            for(int i=0;i<3;i++){
+                String s1=sc.next();
+                int x=sc.nextInt();
+                //Complete this line
+                
+      DecimalFormat df = new DecimalFormat("000");
+      String numberAsString = df.format(x);
+               // System.out.println(s1+"               "+numberAsString);
+                System.out.printf("%1$-14s %2$3s", s1, numberAsString);
+                System.out.println();
+            }
+            System.out.println("================================");
+
+    }
+}
