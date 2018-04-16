3a123401098158b8e68bbd56340ad4fefc49e0ef
Sayali Naik <sayalianaik21@gmail.com>
2018-04-15 18:28:14 -0700

    Update 8_Datatypes.java

--- 8_Datatypes.java
+++ 8_Datatypes.java
@@ -2,12 +2,9 @@ import java.util.*;
 import java.io.*;
 
 
-
 class Solution{
     public static void main(String []argh)
     {
-
-
 
         Scanner sc = new Scanner(System.in);
         int t=sc.nextInt();
