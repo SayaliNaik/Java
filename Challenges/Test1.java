d271d54db5409b8f517268ac3c1d9bedcb646edc
Sayali Naik <21sayalinaik@gmail.com>
2021-01-06 23:14:46 -0800

    BinaryTree_InOrder

---
+++ Challenges/Test1.java
@@ -0,0 +1,71 @@
+package BinaryTree;
+
+import java.util.Stack;
+
+//            40
+//           /  \
+//         20   50
+//        / \     \
+//       10  30    60
+//      /   /  \
+//     5   67  78
+
+//output: Print InOrder(left root right)
+// 5 10 20 30 67 78 40 50 60
+public class Test1 {
+    public static void main(String[] args) throws Exception {
+
+        //Construct Binary Tree
+        BinaryTree bt = BinaryTree.create();
+        System.out.println("Nodes in InOrder using recursion");
+        bt.inOrder();
+
+    }
+}
+
+class BinaryTree {
+    static class TreeNode {
+        String data;
+        TreeNode left, right;
+
+        TreeNode(String value) {
+            this.data = value;
+            left = right = null;
+        }
+    }
+
+    TreeNode root;
+
+    public void inOrder() {
+        inOrder(root);
+    }
+
+    private void inOrder(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+
+        inOrder(node.left);
+        System.out.printf("%s ", node.data);
+        inOrder(node.right);
+    }
+
+
+    public static BinaryTree create() {
+        BinaryTree tree = new BinaryTree();
+        TreeNode root = new TreeNode("40");
+        tree.root = root;
+        tree.root.left = new TreeNode("20");
+        tree.root.left.left = new TreeNode("10");
+        tree.root.left.left.left = new TreeNode("5");
+
+        tree.root.left.right = new TreeNode("30");
+        tree.root.right = new TreeNode("50");
+        tree.root.right.right = new TreeNode("60");
+        tree.root.left.right.left = new TreeNode("67");
+        tree.root.left.right.right = new TreeNode("78");
+
+        return tree;
+
+    }
+}
