ebfdef0ef783b71f1030add35199a8fef5b7804b
ankit komawar <komawarankit67@gmail.com>
2021-04-07 00:33:52 -0400

    adding a print statment

--- 1_IntroToJava.java
+++ 1_IntroToJava.java
@@ -4,5 +4,6 @@ public class Solution {
         /* Enter your code here. Print output to STDOUT. Your class should be named Solution. */
         System.out.println("Hello, World.");
         System.out.println("Hello, Java.");
+        System.out.println("Hello, Sayali.");
     }
 }
